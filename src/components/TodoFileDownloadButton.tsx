ef213829517bff5fbbc9a79423b342e3272c6f92
sodenn <mail@sodenn.dev>
2021-10-31 18:09:07 +0100

    feat: show keyboard shortcuts for the most important actions.

--- src/components/TodoFileDownloadButton.tsx
+++ src/components/TodoFileDownloadButton.tsx
@@ -1,21 +1,38 @@
 import DownloadIcon from "@mui/icons-material/Download";
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useTask } from "../data/TaskContext";
+import { useAddShortcutListener } from "../utils/shortcuts";
+import { Kbd } from "./Kbd";
 
 const TodoFileDownloadButton = () => {
+  const { t } = useTranslation();
   const { downloadTodoFile } = useTask();
+
+  useAddShortcutListener(() => {
+    downloadTodoFile();
+  }, ["t"]);
+
   return (
-    <IconButton
-      tabIndex={-1}
-      aria-label="Download todo.txt"
-      size="large"
-      edge="end"
-      color="inherit"
-      onClick={() => downloadTodoFile()}
+    <Tooltip
+      title={
+        <>
+          {t("Download todo.txt")} <Kbd>T</Kbd>
+        </>
+      }
     >
-      <DownloadIcon />
-    </IconButton>
+      <IconButton
+        tabIndex={-1}
+        aria-label="Download todo.txt"
+        size="large"
+        edge="end"
+        color="inherit"
+        onClick={() => downloadTodoFile()}
+      >
+        <DownloadIcon />
+      </IconButton>
+    </Tooltip>
   );
 };
 
