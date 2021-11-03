2da61ed8133b9a30de5e18da7bc56e32a653e6cf
sodenn <mail@sodenn.dev>
2021-11-03 20:25:31 +0100

    chore: add hygiene step to test workflow + format code

--- src/utils/Context.tsx
+++ src/utils/Context.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export function createContext<T, C>(
   contextFactory: (initialData?: C) => T
-): [(props: { children?: React.ReactNode; initialData?: C }) => JSX.Element, () => T] {
+): [
+  (props: { children?: React.ReactNode; initialData?: C }) => JSX.Element,
+  () => T
+] {
   const Ctx = React.createContext<T>(null as unknown as T);
 
-  const CtxProvider = (props: { children?: React.ReactNode; initialData?: C }) => {
+  const CtxProvider = (props: {
+    children?: React.ReactNode;
+    initialData?: C;
+  }) => {
     const { children, initialData } = props;
     const ctx = contextFactory(initialData);
     return <Ctx.Provider value={ctx}>{children}</Ctx.Provider>;
