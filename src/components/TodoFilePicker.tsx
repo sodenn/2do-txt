a36150b13d1c10e8b357e97db1041b76402b4bbb
sodenn <mail@sodenn.dev>
2021-11-07 17:19:24 +0100

    feat(TodoFilePicker): improve error handling when loading todo.txt files

--- src/components/TodoFilePicker.tsx
+++ src/components/TodoFilePicker.tsx
@@ -1,6 +1,9 @@
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
-import { Button, styled } from "@mui/material";
-import React, { ChangeEvent, PropsWithChildren } from "react";
+import { LoadingButton } from "@mui/lab";
+import { styled } from "@mui/material";
+import { useSnackbar } from "notistack";
+import React, { ChangeEvent, PropsWithChildren, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useTask } from "../data/TaskContext";
 import { usePlatform } from "../utils/platform";
 import { Task } from "../utils/task";
@@ -22,46 +25,79 @@ const TodoFilePicker = ({
     useTask();
   const platform = usePlatform();
   const id = generateId();
+  const { t } = useTranslation();
+  const { enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState(false);
+
+  const readFileContent = async (content: string, file: File) => {
+    let taskList: Task[];
+    // Note: Electron adds a path property to the file object
+    if (platform === "electron") {
+      // Load the content and save the path so that the file can be loaded the next time the app is opened.
+      taskList = await loadTodoFile(content, (file as any).path);
+    } else {
+      // Other platforms does not allow to accessing the file storage. For this reason, a copy of
+      // the selected file is created in the app's document directory.
+      taskList = await saveTodoFile(content);
+    }
+    scheduleDueTaskNotifications(taskList);
+  };
 
   const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-      const fileReader = new FileReader();
-      fileReader.onloadend = async () => {
-        const content = fileReader.result;
-
-        if (typeof content !== "string") {
-          return;
-        }
-
-        let taskList: Task[];
-        if (platform === "electron") {
-          // Note: Electron adds a path property to the file object
-          taskList = await loadTodoFile(content, (file as any).path);
-        } else {
-          taskList = await saveTodoFile(content);
-        }
-        scheduleDueTaskNotifications(taskList);
-
-        if (onSelect) {
-          onSelect();
-        }
-      };
-      fileReader.readAsText(file);
+    if (!event.target.files || event.target.files.length === 0) {
+      return;
     }
+
+    setLoading(true);
+
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = async () => {
+      const content = fileReader.result;
+
+      if (typeof content !== "string") {
+        return;
+      }
+
+      await readFileContent(content, file).catch(() => {
+        enqueueSnackbar(t("The file could not be opened"), {
+          variant: "error",
+        });
+        setLoading(false);
+      });
+
+      if (onSelect) {
+        onSelect();
+      }
+    };
+
+    fileReader.onerror = () => {
+      enqueueSnackbar(t("The file could not be opened"), {
+        variant: "error",
+      });
+      setLoading(false);
+    };
+
+    fileReader.onabort = () => {
+      setLoading(false);
+    };
+
+    fileReader.readAsText(file);
   };
 
   return (
     <label style={{ width: "100%" }} htmlFor={id}>
       <Input accept="text/plain" id={id} type="file" onChange={handleChange} />
-      <Button
+      <LoadingButton
+        loading={loading}
         startIcon={<FolderOpenIcon />}
         fullWidth
         variant="outlined"
         component="span"
       >
         {children}
-      </Button>
+      </LoadingButton>
     </label>
   );
 };
