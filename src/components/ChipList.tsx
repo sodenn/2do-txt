fbee713130d65f8a49e605a40c79f970556477c5
Dennis Soehnen <mail@sodenn.dev>
2021-11-13 17:34:43 +0100

    style(ChipList): replace inline-style with styled component (#109)

--- src/components/ChipList.tsx
+++ src/components/ChipList.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Chip } from "@mui/material";
+import { Badge, Chip, styled } from "@mui/material";
 import React from "react";
 import { Dictionary } from "../utils/types";
 
@@ -9,46 +9,45 @@ interface ChipListProps {
   color?: "info" | "success" | "warning" | "secondary";
 }
 
-const ChipList = ({
-  list = {},
-  selected = [],
-  onClick,
-  color,
-}: ChipListProps) => {
+const List = styled("ul")`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const ListItem = styled("li")`
+  margin-right: ${({ theme }) => theme.spacing(1)};
+  margin-bottom: ${({ theme }) => theme.spacing(0.5)};
+`;
+
+const ChipList = (props: ChipListProps) => {
+  const { list = {}, selected = [], onClick, color } = props;
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        listStyle: "none",
-        p: 0,
-        m: 0,
-      }}
-      component="ul"
-    >
-      {Object.entries(list).map(([item, usages], index) => {
-        return (
-          <Box sx={{ mr: 1, mb: 0.5 }} component="li" key={index}>
-            <Badge
-              badgeContent={usages === 1 ? undefined : usages}
-              color="primary"
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-              }}
-            >
-              <Chip
-                sx={{ px: 1 }}
-                variant={selected.includes(item) ? "filled" : "outlined"}
-                label={item}
-                color={color}
-                onClick={() => onClick && onClick(item)}
-              />
-            </Badge>
-          </Box>
-        );
-      })}
-    </Box>
+    <List>
+      {Object.entries(list).map(([item, usages], index) => (
+        <ListItem key={index}>
+          <Badge
+            badgeContent={usages === 1 ? undefined : usages}
+            color="primary"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Chip
+              sx={{ px: 1 }}
+              variant={selected.includes(item) ? "filled" : "outlined"}
+              label={item}
+              color={color}
+              onClick={() => onClick && onClick(item)}
+            />
+          </Badge>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
