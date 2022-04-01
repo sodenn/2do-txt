103819e77bd2f0ee7e1b1233a58639f6f493a051
Dennis Soehnen <mail@sodenn.dev>
2022-04-01 23:05:09 +0200

    feat(PrioritySelect): better keyboard support for task priority selection (#308)

--- src/data/ProviderBundle.tsx
+++ src/data/ProviderBundle.tsx
@@ -1,6 +1,7 @@
 import { Grow } from "@mui/material";
 import { SnackbarProvider } from "notistack";
 import { FC, Suspense } from "react";
+import { AppTheme } from "./AppThemeContext";
 import { CloudStorageProvider } from "./CloudStorageContext";
 import { ConfirmationDialogProvider } from "./ConfirmationDialogContext";
 import { FileCreateDialogProvider } from "./FileCreateDialogContext";
@@ -16,43 +17,45 @@ import { TaskDialogProvider } from "./TaskDialogContext";
 
 const ProviderBundle: FC = ({ children }) => {
   return (
-    <SnackbarProvider
-      maxSnack={3}
-      preventDuplicate={true}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "right",
-      }}
-      TransitionComponent={Grow as any}
-    >
-      <Suspense fallback={null}>
-        <LoadingProvider>
-          <MigrationProvider>
-            <ConfirmationDialogProvider>
-              <NetworkProvider>
-                <CloudStorageProvider>
-                  <SettingsProvider>
-                    <FilterProvider>
-                      <SideSheetProvider>
-                        <TaskProvider>
-                          <FileManagementDialogProvider>
-                            <FileCreateDialogProvider>
-                              <TaskDialogProvider>
-                                {children}
-                              </TaskDialogProvider>
-                            </FileCreateDialogProvider>
-                          </FileManagementDialogProvider>
-                        </TaskProvider>
-                      </SideSheetProvider>
-                    </FilterProvider>
-                  </SettingsProvider>
-                </CloudStorageProvider>
-              </NetworkProvider>
-            </ConfirmationDialogProvider>
-          </MigrationProvider>
-        </LoadingProvider>
-      </Suspense>
-    </SnackbarProvider>
+    <Suspense fallback={null}>
+      <AppTheme>
+        <SnackbarProvider
+          maxSnack={3}
+          preventDuplicate={true}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          TransitionComponent={Grow as any}
+        >
+          <LoadingProvider>
+            <MigrationProvider>
+              <ConfirmationDialogProvider>
+                <NetworkProvider>
+                  <CloudStorageProvider>
+                    <SettingsProvider>
+                      <FilterProvider>
+                        <SideSheetProvider>
+                          <TaskProvider>
+                            <FileManagementDialogProvider>
+                              <FileCreateDialogProvider>
+                                <TaskDialogProvider>
+                                  {children}
+                                </TaskDialogProvider>
+                              </FileCreateDialogProvider>
+                            </FileManagementDialogProvider>
+                          </TaskProvider>
+                        </SideSheetProvider>
+                      </FilterProvider>
+                    </SettingsProvider>
+                  </CloudStorageProvider>
+                </NetworkProvider>
+              </ConfirmationDialogProvider>
+            </MigrationProvider>
+          </LoadingProvider>
+        </SnackbarProvider>
+      </AppTheme>
+    </Suspense>
   );
 };
 
