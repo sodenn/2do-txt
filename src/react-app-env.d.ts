33d75d9aa9f8417186f8f7784d19e6c198381274
sodenn <mail@sodenn.dev>
2022-04-17 18:34:32 +0200

    chore: update ProcessEnv

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,7 +3,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
     PUBLIC_URL: string;
-    REACT_APP_BASE_PATH?: string;
     REACT_APP_DROPBOX_CLIENT_ID: string;
     REACT_APP_WEB_CLOUD_STORAGE_ENABLE: "true" | "false";
     REACT_APP_DEFAULT_FILE_NAME: string;
