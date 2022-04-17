b1933edd1b546367827b966ff5df778f4329644d
sodenn <mail@sodenn.dev>
2022-04-17 18:11:06 +0200

    build: remove redundant environment variables

--- src/utils/routing.ts
+++
@@ -1,7 +0,0 @@
-export function getBaseUrl() {
-  if (process.env.REACT_APP_BASE_PATH) {
-    return window.location.origin + process.env.REACT_APP_BASE_PATH;
-  } else {
-    return window.location.origin;
-  }
-}
