8db1c0a4879433803266b60c1b226fcc53bdd717
sodenn <mail@sodenn.dev>
2022-04-29 22:22:07 +0200

    update vercel config

--- vercel.sh
+++ vercel.sh
@@ -3,14 +3,12 @@
 echo "VERCEL_GIT_COMMIT_AUTHOR_LOGIN: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ $VERCEL_GIT_COMMIT_REF == main ]] || \
-   [[ $VERCEL_GIT_COMMIT_REF == feat* ]] || \
-   [[ $VERCEL_GIT_COMMIT_REF == build* ]] ; then
-  # Proceed with the build
-  echo "✅ - Build can proceed"
-  exit 1;
-else
+if [[ $VERCEL_GIT_COMMIT_REF == chore* ]] ; then
   # Don't build
   echo "🛑 - Build cancelled"
   exit 0;
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
 fi
