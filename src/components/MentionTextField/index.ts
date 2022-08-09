c8394f20e46a263593f6602a2a0b6ed5e5652b03
sodenn <mail@sodenn.dev>
2022-08-09 22:01:44 +0200

    refactor: use react-fluent-edit instead of MentionTextField

--- src/components/MentionTextField/index.ts
+++
@@ -1,4 +0,0 @@
-import MuiMentionTextField from "./MuiMentionTextField";
-import { useMentionTextField } from "./use-mention-text-field";
-export { default } from "./MentionTextField";
-export { MuiMentionTextField, useMentionTextField };
