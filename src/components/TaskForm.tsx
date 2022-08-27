dc5c5b85db6c7357ca91882dff2da699d17737a8
sodenn <mail@sodenn.dev>
2022-08-27 15:41:05 +0200

    fix eslint issue

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -69,8 +69,7 @@ const TaskForm = (props: TaskFormProps) => {
     !(showCreationDate && showCompletionDate)
       ? 4
       : 6;
-  const { openMentionsCombobox, removeMentions, renameMentions, addMention } =
-    useMentions();
+  const { openMentionsCombobox, removeMentions, addMention } = useMentions();
   const plugins = useMemo(
     () => [
       createMentionsPlugin({
