3ee4d0c4d6167998ac3e024569db680ff9d3e860
sodenn <mail@sodenn.dev>
2022-09-10 13:36:38 +0200

    delete dist folder

--- dist/assets/web.491d8ab7.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index.8fe19ad3.js";class t extends e{async requestReview(){}}export{t as RateAppWeb};
--- dist/assets/web.6f4a8b48.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index.8fe19ad3.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
