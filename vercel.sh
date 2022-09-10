ffea23ee4450f23c88f13ec7f5c3b83b56ac0d81
sodenn <mail@sodenn.dev>
2022-09-10 11:56:06 +0200

    update slate-react to v0.82.0

--- vercel.sh
+++ vercel.sh
@@ -6,6 +6,7 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 if [[ $VERCEL_GIT_COMMIT_REF == main ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == feat* ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == chore* ]] || \
+   [[ $VERCEL_GIT_COMMIT_REF == fix* ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == build* ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
