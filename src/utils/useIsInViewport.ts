6c823556c8e8f305feaf2b3e98c446b563d0c68c
sodenn <mail@sodenn.dev>
2022-12-12 21:02:06 +0100

    add large todo.txt for testing purposes

--- src/utils/useIsInViewport.ts
+++ src/utils/useIsInViewport.ts
@@ -11,33 +11,27 @@ export function useIsInViewport(elem: Element) {
 
   const observer = useMemo(
     () =>
-      new IntersectionObserver(
-        ([entry]) => {
-          const currentY = entry.boundingClientRect.y;
-          const currentRatio = entry.intersectionRatio;
-          const isIntersecting = entry.isIntersecting;
-          const { y: previousY, ratio: previousRatio } = previous.current;
-          const newDirection =
-            currentY > previousY && currentRatio < previousRatio
-              ? "above"
-              : currentY < previousY
-              ? "below"
-              : undefined;
-          setValue((curr) => ({
-            visible: isIntersecting,
-            ...(isIntersecting
-              ? { direction: curr.direction }
-              : {
-                  direction: newDirection,
-                }),
-          }));
-          previous.current = { y: currentY, ratio: currentRatio };
-        },
-        {
-          root: null,
-          threshold: 0,
-        }
-      ),
+      new IntersectionObserver(([entry]) => {
+        const currentY = entry.boundingClientRect.y;
+        const currentRatio = entry.intersectionRatio;
+        const isIntersecting = entry.isIntersecting;
+        const { y: previousY, ratio: previousRatio } = previous.current;
+        const newDirection =
+          currentY > previousY && currentRatio < previousRatio
+            ? "above"
+            : currentY < previousY
+            ? "below"
+            : undefined;
+        setValue((curr) => ({
+          visible: isIntersecting,
+          ...(isIntersecting
+            ? { direction: curr.direction }
+            : {
+                direction: newDirection,
+              }),
+        }));
+        previous.current = { y: currentY, ratio: currentRatio };
+      }),
     []
   );
 
