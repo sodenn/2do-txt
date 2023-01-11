d33d86b4bd251bb7125c6a9e6a3fc400700c3689
sodenn <mail@sodenn.dev>
2023-01-11 23:06:25 +0100

    close tauri splash screen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,21 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+// This command must be async so that it doesn't run on the main thread.
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+      splashscreen.close().unwrap();
+  }
+  // Show main window
+  window.get_window("main").unwrap().show().unwrap();
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/utils/splash-screen.ts
+++ src/utils/splash-screen.ts
@@ -1,12 +1,11 @@
 import { SplashScreen } from "@capacitor/splash-screen";
+import { invoke } from "@tauri-apps/api/tauri";
 import { getPlatform } from "./platform";
 
 export function hideSplashScreen() {
   const platform = getPlatform();
   SplashScreen.hide();
   if (platform === "desktop") {
-    setTimeout(() => {
-      window.electron.hideSplashScreen();
-    }, 200);
+    invoke("close_splashscreen");
   }
 }
