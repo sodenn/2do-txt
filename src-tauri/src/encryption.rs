8f9d436257c0dc57352a0d2ae5da4b920bd50c75
sodenn <mail@sodenn.dev>
2023-01-15 21:17:10 +0100

    format code

--- src-tauri/src/encryption.rs
+++ src-tauri/src/encryption.rs
@@ -1,57 +1,60 @@
 use std::str;
 
-use aes_gcm::{aead::{Aead, KeyInit}, Aes256Gcm, AesGcm, Key, Nonce};
 use aes_gcm::aead::consts::U12;
 use aes_gcm::aes::Aes256;
-use keyring::{Entry,Error};
-use rand::{distributions::Alphanumeric, Rng, thread_rng};
+use aes_gcm::{
+    aead::{Aead, KeyInit},
+    Aes256Gcm, AesGcm, Key, Nonce,
+};
+use keyring::{Entry, Error};
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
 
 const NONCE_LENGTH: usize = 12;
 static SERVICE: &'static str = "2do.txt Encryption Key";
 static USERNAME: &'static str = "2do.txt";
 
 fn retrieve_key() -> String {
-  let entry = Entry::new(SERVICE, USERNAME);
-  let key = match entry.get_password() {
-    Ok(val) => val,
-    Err(error) => match error {
-      Error::NoEntry => {
-        let new_key: String = thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(32)
-            .map(|x| x as char)
-            .collect();
-        entry.set_password(&new_key).unwrap();
-        return new_key
-      },
-      other_error => {
-        panic!("Problem retrieving key: {:?}", other_error);
-      }
-    },
-  };
-  key
+    let entry = Entry::new(SERVICE, USERNAME);
+    let key = match entry.get_password() {
+        Ok(val) => val,
+        Err(error) => match error {
+            Error::NoEntry => {
+                let new_key: String = thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(32)
+                    .map(|x| x as char)
+                    .collect();
+                entry.set_password(&new_key).unwrap();
+                return new_key;
+            }
+            other_error => {
+                panic!("Problem retrieving key: {:?}", other_error);
+            }
+        },
+    };
+    key
 }
 
 fn create_cipher() -> AesGcm<Aes256, U12> {
-  let raw_key = retrieve_key();
-  let key = Key::<Aes256Gcm>::from_slice(&raw_key.as_bytes());
-  Aes256Gcm::new(key)
+    let raw_key = retrieve_key();
+    let key = Key::<Aes256Gcm>::from_slice(&raw_key.as_bytes());
+    Aes256Gcm::new(key)
 }
 
 pub fn encrypt(plaintext: &[u8]) -> Vec<u8> {
-  let cipher = create_cipher();
-  let rnd: [u8; NONCE_LENGTH] = thread_rng().gen();
-  let nonce = Nonce::from_slice(&rnd);
-  let encrypted = cipher.encrypt(nonce, plaintext).unwrap();
-  let mut ciphertext = Vec::new();
-  ciphertext.extend_from_slice(&rnd);
-  ciphertext.extend(encrypted);
-  ciphertext
+    let cipher = create_cipher();
+    let rnd: [u8; NONCE_LENGTH] = thread_rng().gen();
+    let nonce = Nonce::from_slice(&rnd);
+    let encrypted = cipher.encrypt(nonce, plaintext).unwrap();
+    let mut ciphertext = Vec::new();
+    ciphertext.extend_from_slice(&rnd);
+    ciphertext.extend(encrypted);
+    ciphertext
 }
 
 pub fn decrypt(encrypted: &[u8]) -> String {
-  let cipher = create_cipher();
-  let nonce = Nonce::from_slice(&encrypted[0..NONCE_LENGTH]);
-  let decrypted = cipher.decrypt(nonce, &encrypted[NONCE_LENGTH..]).unwrap();
-  String::from_utf8(decrypted.clone()).unwrap()
+    let cipher = create_cipher();
+    let nonce = Nonce::from_slice(&encrypted[0..NONCE_LENGTH]);
+    let decrypted = cipher.decrypt(nonce, &encrypted[NONCE_LENGTH..]).unwrap();
+    String::from_utf8(decrypted.clone()).unwrap()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,13 @@
 #![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use tauri::Manager;
 
-use secure_storage::{get_secure_storage_item, remove_secure_storage_item, set_secure_storage_item};
+use secure_storage::{
+    get_secure_storage_item, remove_secure_storage_item, set_secure_storage_item,
+};
 
 mod encryption;
 mod secure_storage;
@@ -13,22 +15,22 @@ mod secure_storage;
 // This command must be async so that it doesn't run on the main thread.
 #[tauri::command]
 async fn close_splashscreen(window: tauri::Window) {
-  // Close splashscreen
-  if let Some(splashscreen) = window.get_window("splashscreen") {
-    splashscreen.close().unwrap();
-  }
-  // Show main window
-  window.get_window("main").unwrap().show().unwrap();
+    // Close splashscreen
+    if let Some(splashscreen) = window.get_window("splashscreen") {
+        splashscreen.close().unwrap();
+    }
+    // Show main window
+    window.get_window("main").unwrap().show().unwrap();
 }
 
 fn main() {
-  tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![
-        close_splashscreen,
-        get_secure_storage_item,
-        set_secure_storage_item,
-        remove_secure_storage_item
-      ])
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            close_splashscreen,
+            get_secure_storage_item,
+            set_secure_storage_item,
+            remove_secure_storage_item
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
