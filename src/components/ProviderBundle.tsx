c51633e1093cfc1a1fa8d04eccfcbfd344e78ab5
sodenn <mail@sodenn.dev>
2023-04-12 19:48:35 +0200

    fix snackbar styling

--- src/components/ProviderBundle.tsx
+++ src/components/ProviderBundle.tsx
@@ -10,27 +10,29 @@ const ProviderBundle = ({ children }: WithChildren) => {
   const loaderData = useLoaderData() as AppLoaderData;
   return (
     <Suspense fallback={null}>
-      <SnackbarProvider
-        maxSnack={3}
-        preventDuplicate={true}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right",
-        }}
-        TransitionComponent={Grow}
+      <AppStoreProvider
+        filter={loaderData.filter}
+        settings={loaderData.settings}
+        platform={loaderData.platform}
+        theme={loaderData.theme}
+        task={loaderData.task}
+        cloud={loaderData.cloud}
+        network={loaderData.network}
       >
-        <AppStoreProvider
-          filter={loaderData.filter}
-          settings={loaderData.settings}
-          platform={loaderData.platform}
-          theme={loaderData.theme}
-          task={loaderData.task}
-          cloud={loaderData.cloud}
-          network={loaderData.network}
-        >
-          <AppThemeProvider>{children}</AppThemeProvider>
-        </AppStoreProvider>
-      </SnackbarProvider>
+        <AppThemeProvider>
+          <SnackbarProvider
+            maxSnack={3}
+            preventDuplicate={true}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            TransitionComponent={Grow}
+          >
+            {children}
+          </SnackbarProvider>
+        </AppThemeProvider>
+      </AppStoreProvider>
     </Suspense>
   );
 };
