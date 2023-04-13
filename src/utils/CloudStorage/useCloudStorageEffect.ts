055ac150754651c96f73c6451256c36d914054c2
sodenn <mail@sodenn.dev>
2023-04-14 00:27:49 +0200

    add German translation

--- src/utils/CloudStorage/useCloudStorageEffect.ts
+++ src/utils/CloudStorage/useCloudStorageEffect.ts
@@ -1,6 +1,4 @@
-import { useTheme } from "@mui/material";
 import { differenceInMinutes } from "date-fns";
-import PullToRefresh from "pulltorefreshjs";
 import { useCallback, useEffect } from "react";
 import {
   CreateFileData,
@@ -10,65 +8,16 @@ import {
 } from "../../native-api/filesystem";
 import {
   addBecomeActiveListener,
-  hasTouchScreen,
   removeAllBecomeActiveListeners,
 } from "../../native-api/platform";
 import { taskLoader } from "../../stores/task-state";
 import { parseDate } from "../date";
 import { getTodoFilePathFromDoneFilePath, isDoneFilePath } from "../todo-files";
 import useArchivedTask from "../useArchivedTask";
+import usePullToRefresh from "../usePullToRefresh";
 import useTask from "../useTask";
 import { cloudStoragePreferences } from "./preferences";
 import { useCloudStorage } from "./useCloudStorage";
-
-const ptrStyles = `
-.__PREFIX__ptr {
-  box-shadow: inset 0 -3px 5px rgba(0, 0, 0, 0.12);
-  pointer-events: none;
-  font-size: 0.85em;
-  font-weight: bold;
-  top: 0;
-  height: 0;
-  transition: height 0.3s, min-height 0.3s;
-  text-align: center;
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-end;
-  align-content: stretch;
-}
-
-.__PREFIX__box {
-  padding: 10px;
-  flex-basis: 100%;
-}
-
-.__PREFIX__pull {
-  transition: none;
-}
-
-.__PREFIX__text {
-  margin-top: .33em;
-  color: rgba(0, 0, 0, 0.3);
-}
-
-.__PREFIX__icon {
-  color: rgba(0, 0, 0, 0.3);
-  transition: transform .3s;
-}
-
-/*
-When at the top of the page, disable vertical overscroll so passive touch
-listeners can take over.
-*/
-.__PREFIX__top {
-  touch-action: pan-x pan-down pinch-zoom;
-}
-
-.__PREFIX__release .__PREFIX__icon {
-  transform: rotate(180deg);
-}
-`;
 
 export function useCloudStorageEffect() {
   const {
@@ -81,7 +30,6 @@ export function useCloudStorageEffect() {
   } = useCloudStorage();
   const { syncDoneFiles } = useArchivedTask();
   const { loadTodoFile } = useTask();
-  const theme = useTheme();
 
   const reloadTodoFile = useCallback(
     async (path: string, content?: string) => {
@@ -187,26 +135,9 @@ export function useCloudStorageEffect() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Sync files with cloud storage when pull to refresh
-  useEffect(() => {
-    const touchScreen = hasTouchScreen();
-    if (!touchScreen || cloudStorages.length > 0) {
-      return;
-    }
-    const instance = PullToRefresh.init({
-      mainElement: "#scroll-container",
-      getStyles() {
-        return ptrStyles.replaceAll(
-          "color: rgba(0, 0, 0, 0.3);",
-          `color: ${theme.palette.text.disabled};`
-        );
-      },
-      onRefresh() {
-        return syncAllTodoFiles();
-      },
-    });
-    return () => {
-      instance.destroy();
-    };
-  }, [cloudStorages.length, syncAllTodoFiles, theme.palette.text.disabled]);
+  usePullToRefresh(
+    syncAllTodoFiles,
+    "#scroll-container",
+    cloudStorages.length === 0
+  );
 }
