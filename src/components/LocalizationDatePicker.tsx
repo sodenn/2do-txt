9b1920e14c9fc1bb3514259f3e5cc07ff91a6872
sodenn <mail@sodenn.dev>
2023-06-19 23:25:46 +0200

    chore(deps): update @mui/x-date-pickers to v6.8.0

--- src/components/LocalizationDatePicker.tsx
+++ src/components/LocalizationDatePicker.tsx
@@ -1,15 +1,16 @@
-import { TextField, useMediaQuery } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import {
   DatePicker,
   DatePickerProps,
+  FieldSelectedSections,
   LocalizationProvider,
   deDE,
 } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { Locale } from "date-fns";
+import { Locale, isAfter, isBefore, isValid } from "date-fns";
 import deLocale from "date-fns/locale/de";
 import enLocale from "date-fns/locale/en-US";
-import { forwardRef } from "react";
+import { KeyboardEvent, MouseEvent, forwardRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const localeMap: Record<string, Locale> = {
@@ -17,15 +18,16 @@ const localeMap: Record<string, Locale> = {
   de: deLocale,
 };
 
-const getLocalText = (language: string) =>
-  language === "de"
-    ? deDE.components.MuiLocalizationProvider.defaultProps.localeText
-    : undefined;
+type LocalizationDatePickerProps = DatePickerProps<Date> & {
+  ariaLabel?: string;
+};
 
-type LocalizationDatePickerProps = Omit<
-  DatePickerProps<Date | undefined, Date>,
-  "renderInput" | "date" | "openPicker" | "rawValue"
-> & { ariaLabel?: string };
+const minDate = new Date("1900-01-01T00:00:00.000");
+const maxDate = new Date("2099-12-31T00:00:00.000");
+
+function isValidDate(date: Date) {
+  return isValid(date) && isAfter(date, minDate) && isBefore(date, maxDate);
+}
 
 const LocalizationDatePicker = forwardRef<
   HTMLInputElement,
@@ -33,49 +35,88 @@ const LocalizationDatePicker = forwardRef<
 >((props, ref) => {
   const { value = null, ariaLabel, onChange, ...rest } = props;
   const desktop = useMediaQuery("@media (pointer: fine)");
+  const [selectedSections, setSelectedSections] =
+    useState<FieldSelectedSections>(null);
+
+  const handleDoubleClick = (event: MouseEvent) => {
+    if (!desktop) {
+      return;
+    }
+    (event.target as any)?.focus();
+    setSelectedSections("all");
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (!desktop) {
+      return;
+    }
+    if (event.key === "Backspace" && selectedSections === "all") {
+      setSelectedSections(0);
+    }
+    if (event.key === "Backspace" && typeof selectedSections === "number") {
+      setSelectedSections(Math.max(selectedSections - 1, 0));
+    }
+  };
 
   const {
-    i18n: { resolvedLanguage = "en" },
+    i18n: { language },
   } = useTranslation();
+
+  const handleChange: DatePickerProps<Date>["onChange"] = (value, context) => {
+    if (!value || isValidDate(value)) {
+      onChange?.(value, context);
+    }
+  };
 
   return (
     <LocalizationProvider
-      localeText={getLocalText(resolvedLanguage)}
+      localeText={
+        language === "de"
+          ? deDE.components.MuiLocalizationProvider.defaultProps.localeText
+          : undefined
+      }
       dateAdapter={AdapterDateFns}
-      adapterLocale={localeMap[resolvedLanguage]}
+      adapterLocale={localeMap[language]}
     >
       <DatePicker
         {...rest}
-        onChange={onChange}
+        minDate={minDate}
+        maxDate={maxDate}
+        onChange={handleChange}
         ref={ref}
         value={value}
-        PopperProps={{
-          modifiers: [
-            {
-              name: "flip",
-              options: {
-                altBoundary: false,
-                fallbackPlacements: ["right", "left"],
-              },
+        selectedSections={selectedSections}
+        onSelectedSectionsChange={setSelectedSections}
+        slotProps={{
+          textField: (props) => ({
+            fullWidth: true,
+            inputProps: {
+              ...props.inputProps,
+              "data-testid": `${ariaLabel} textfield`,
+              "aria-label": ariaLabel,
             },
-          ],
+            onDoubleClick: handleDoubleClick,
+            onKeyDown: handleKeyDown,
+          }),
+          openPickerButton: {
+            // @ts-ignore
+            "data-testid": `${ariaLabel} pickerbutton`,
+          },
+          actionBar: {
+            actions: desktop ? ["clear"] : ["accept", "cancel", "clear"],
+          },
+          popper: {
+            modifiers: [
+              {
+                name: "flip",
+                options: {
+                  altBoundary: false,
+                  fallbackPlacements: ["right", "left"],
+                },
+              },
+            ],
+          },
         }}
-        componentsProps={
-          desktop
-            ? {
-                actionBar: { actions: ["clear"] },
-              }
-            : {
-                actionBar: { actions: ["accept", "cancel", "clear"] },
-              }
-        }
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            fullWidth
-            inputProps={{ ...params.inputProps, "aria-label": ariaLabel }}
-          />
-        )}
       />
     </LocalizationProvider>
   );
