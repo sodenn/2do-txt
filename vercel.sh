3cc4c610f26e9abcb17a9faeee21d8eec71bd675
sodenn <mail@sodenn.dev>
2023-09-13 23:12:11 +0200

    update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -5,6 +5,8 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ $VERCEL_GIT_COMMIT_REF == main ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == feat* ]] || \
+   [[ $VERCEL_GIT_COMMIT_REF == style* ]] || \
+   [[ $VERCEL_GIT_COMMIT_REF == test* ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == chore* ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == fix* ]] || \
    [[ $VERCEL_GIT_COMMIT_REF == refactor* ]] || \
