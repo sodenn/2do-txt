d6096e9e0565caa44db44626383cc935eeca774c
sodenn <mail@sodenn.dev>
2023-09-16 10:59:01 +0200

    re-add @mui/system dependency

--- src/utils/useMediaQuery.ts
+++ src/utils/useMediaQuery.ts
@@ -1,32 +1,63 @@
-import { useEffect, useState } from "react";
+import { getThemeProps, useThemeWithoutDefault as useTheme } from "@mui/system";
+import * as React from "react";
+import { useSyncExternalStore } from "react";
 
-export function useMediaQuery(query: string): boolean {
-  const getMatches = (query: string): boolean => {
-    // Prevents SSR issues
-    if (typeof window !== "undefined") {
-      return window.matchMedia(query).matches;
+interface UseMediaQueryOptions {
+  defaultMatches?: boolean;
+  matchMedia?: typeof window.matchMedia;
+  noSsr?: boolean;
+  ssrMatchMedia?: (query: string) => { matches: boolean };
+}
+
+export function useMediaQuery<Theme = unknown>(
+  queryInput: string | ((theme: Theme) => string),
+  options: UseMediaQueryOptions = {},
+): boolean {
+  const theme = useTheme<Theme>();
+  const supportMatchMedia =
+    typeof window !== "undefined" && typeof window.matchMedia !== "undefined";
+  const {
+    defaultMatches = false,
+    matchMedia = supportMatchMedia ? window.matchMedia : null,
+    ssrMatchMedia = null,
+    noSsr = false,
+  } = getThemeProps({ name: "MuiUseMediaQuery", props: options, theme });
+
+  let query = typeof queryInput === "function" ? queryInput(theme) : queryInput;
+  query = query.replace(/^@media( ?)/m, "");
+
+  const getDefaultSnapshot = React.useCallback(
+    () => defaultMatches,
+    [defaultMatches],
+  );
+
+  const getServerSnapshot = React.useMemo(() => {
+    if (noSsr && matchMedia) {
+      return () => matchMedia!(query).matches;
     }
-    return false;
-  };
 
-  const [matches, setMatches] = useState<boolean>(getMatches(query));
+    if (ssrMatchMedia !== null) {
+      const { matches } = ssrMatchMedia(query);
+      return () => matches;
+    }
+    return getDefaultSnapshot;
+  }, [getDefaultSnapshot, query, ssrMatchMedia, noSsr, matchMedia]);
 
-  useEffect(() => {
-    const matchMedia = window.matchMedia(query);
+  const [getSnapshot, subscribe] = React.useMemo(() => {
+    if (matchMedia === null) {
+      return [getDefaultSnapshot, () => () => {}];
+    }
+    const mediaQueryList = matchMedia(query);
+    return [
+      () => mediaQueryList.matches,
+      (notify: () => void) => {
+        mediaQueryList.addEventListener("change", notify);
+        return () => {
+          mediaQueryList.addEventListener("change", notify);
+        };
+      },
+    ];
+  }, [getDefaultSnapshot, matchMedia, query]);
 
-    const handleChange = () => {
-      const matches = getMatches(query.replace(/^@media( ?)/m, ""));
-      setMatches(matches);
-    };
-
-    // Triggered at the first client-side load and if query changes
-    handleChange();
-
-    matchMedia.addEventListener("change", handleChange);
-    return () => {
-      matchMedia.removeEventListener("change", handleChange);
-    };
-  }, [query]);
-
-  return matches;
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 }
