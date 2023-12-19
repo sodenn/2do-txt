ccb1282761e4b50534230442e0a56053e3e03a29
sodenn <mail@sodenn.dev>
2023-12-19 19:17:19 +0100

    error handling

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,9 +1,12 @@
 export async function preloadImages(imageUrls: URL[]) {
   const promises = imageUrls.map((url) => {
-    return new Promise<void>((resolve, reject) => {
+    return new Promise<void>((resolve) => {
       const img = new Image();
       img.onload = () => resolve();
-      img.onerror = reject;
+      img.onerror = () => {
+        console.error(`Failed to load image: ${url.href}`);
+        resolve();
+      };
       img.src = url.href;
     });
   });
