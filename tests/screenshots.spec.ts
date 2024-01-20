14c277864a8555c62f24b390a4c2e6c14c5385c5
Dennis Soehnen <mail@sodenn.dev>
2024-01-20 18:20:09 +0100

    test(): set host depending on env var (#971)

--- tests/screenshots.spec.ts
+++ tests/screenshots.spec.ts
@@ -1,7 +1,8 @@
 import { Page, expect, test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+  const host = process.env.HOST || "localhost";
+  await page.goto(`http://${host}:5173/`);
 });
 
 test.describe("Screenshots", () => {
