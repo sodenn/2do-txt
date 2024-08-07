f4fa6c0a7467b19bf6465fbb0fcd4eb1d3df7466
sodenn <mail@sodenn.dev>
2024-08-07 21:47:20 +0200

    remove test code

--- src/components/ReloadPrompt.tsx
+++ src/components/ReloadPrompt.tsx
@@ -1,5 +1,4 @@
 import { useConfirmationDialogStore } from "@/stores/confirmation-dialog-store";
-import { useEffect } from "react";
 import { useRegisterSW } from "virtual:pwa-register/react";
 
 export function ReloadPrompt() {
@@ -27,23 +26,6 @@ export function ReloadPrompt() {
       });
     },
   });
-
-  useEffect(() => {
-    openConfirmationDialog({
-      title: "New Update Available",
-      content: "New content available, click on reload button to update.",
-      buttons: [
-        {
-          text: "Close",
-          variant: "outline",
-        },
-        {
-          text: "Reload",
-          handler() {},
-        },
-      ],
-    });
-  }, []);
 
   return null;
 }
