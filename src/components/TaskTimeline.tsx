d60ae071e6fc35b0c1d8e886492c7230de52b880
sodenn <mail@sodenn.dev>
2024-08-17 19:03:36 +0200

    adjust timeline styles

--- src/components/TaskTimeline.tsx
+++ src/components/TaskTimeline.tsx
@@ -22,10 +22,10 @@ import {
   PlusIcon,
 } from "lucide-react";
 import {
+  forwardRef,
   HTMLAttributes,
   KeyboardEvent,
   MutableRefObject,
-  forwardRef,
   useEffect,
   useState,
 } from "react";
@@ -174,7 +174,10 @@ const TodayItem = forwardRef<HTMLButtonElement, WithTimelineTask>(
             <PlusIcon className="h-4 w-4" />
           </Button>
           <TimelineConnector
-            className={cn("mt-1", !flags.lastOfToday && "border-primary")}
+            className={cn(
+              "-mb-[3px] mt-2",
+              !flags.lastOfToday && "border-primary",
+            )}
           />
         </div>
         <div className="self-start" style={{ gridArea: "content" }}>
@@ -209,7 +212,7 @@ const TaskItem = forwardRef<HTMLDivElement, TaskItemProps>((props, ref) => {
       {!flags.first && (
         <TimelineConnector
           className={cn(
-            "-mt-[2px] mb-1 justify-self-center",
+            "mb-1 justify-self-center",
             flags.today && !flags.firstOfToday && "border-primary",
           )}
           style={{
@@ -299,7 +302,7 @@ function TaskCheckbox({
 }: TaskCheckboxProps) {
   return (
     <div
-      className="flex flex-col items-center justify-center gap-0.5 self-start"
+      className="flex h-full flex-col items-center justify-center gap-0.5 self-start"
       style={{
         gridArea: "action",
       }}
@@ -318,7 +321,7 @@ function TaskCheckbox({
       </Button>
       <TimelineConnector
         className={cn(
-          "mt-1",
+          "-mb-[3px] mt-1 flex-1",
           flags.today && !flags.lastOfToday && "border-primary",
           flags.last ? "invisible" : "visible",
         )}
@@ -389,7 +392,7 @@ const TimelineItem = forwardRef<HTMLDivElement, TimelineItemProps>(
     return (
       <div
         ref={ref}
-        className={cn("-mb-1 -mt-1 grid items-center gap-0.5", className)}
+        className={cn("grid items-center gap-0.5", className)}
         style={{
           gridTemplateColumns: "auto 50px 1fr",
           gridTemplateAreas: gridTemplateAreas,
