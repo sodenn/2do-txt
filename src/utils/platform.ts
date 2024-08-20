c98333ab57d3d9db7b233895a501c1dc04bb2fed
sodenn <mail@sodenn.dev>
2024-08-20 19:37:36 +0200

    dont use ScrollArea for mobile devices

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -31,6 +31,8 @@ export const SUPPORTS_SHOW_OPEN_FILE_PICKER =
 
 export const HAS_TOUCHSCREEN = hasTouchScreen();
 
+export const HAS_HIDDEN_SCROLLBAR = getScrollbarWidth() === 0;
+
 function hasTouchScreen() {
   if ("maxTouchPoints" in navigator) {
     return navigator.maxTouchPoints > 0;
@@ -52,4 +54,24 @@ function hasTouchScreen() {
       );
     }
   }
+}
+
+function getScrollbarWidth() {
+  const outer = document.createElement("div");
+  outer.style.visibility = "hidden";
+  outer.style.width = "100px";
+  document.body.appendChild(outer);
+
+  const widthNoScroll = outer.offsetWidth;
+  outer.style.overflow = "scroll";
+
+  const inner = document.createElement("div");
+  inner.style.width = "100%";
+  outer.appendChild(inner);
+
+  const widthWithScroll = inner.offsetWidth;
+
+  outer.parentNode?.removeChild(outer);
+
+  return widthNoScroll - widthWithScroll;
 }
